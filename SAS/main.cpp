3a84275f9f6d47d20e0a4d23581a2188a643212e
Seri <serianoseri@gmail.com>
2019-11-10 15:32:23 +0100

    new schifomerda in test

--- SAS/main.cpp
+++ SAS/main.cpp
@@ -1,40 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <windows.h>
-#include <stdio.h>
 #include <fstream>
 
 using namespace std;
 
-#define RESET_COLOR "\e[m"
-#define MAKE_GREEN "\e[32m"
+#define Blue 1
+#define Green 2
+#define Cyan 3
+#define Red 4
+#define Purple 5
+#define Yellow 6
+#define Default 7
+#define Gray 8
+#define Bblue 9
+#define Bgreen 10
+#define Bcyan 11
+#define Bred 12
+#define Pink 13
+#define BYellow 14
+#define Bwhite 15
 
 string Color(int color, string Message);
 void getMetodo(string metodo);
+void getHack();
+string getTarget();
 
 int main()
 {
-    ifstream ifs("whatsapp.txt");
-    string content((istreambuf_iterator<char>(ifs)),
-                       (istreambuf_iterator<char>()));
-
-    cout << content;
-    string metodo;
-    cout << MAKE_GREEN "this will show up green" RESET_COLOR "\n";
-    cout << Color(12,"Hey! I'm in color 12!") << endl; //Sets a color and prints the text
-    cout << Color(10," Nice C: 10"); //Sets another color and prints the text
-    //cin >> metodo;
-    //getMetodo(metodo);
-    Color(7, "");
+    getHack();
     return 0;
+}
+
+void getHack()
+{
+    string line;
+    fstream meth{"whatsapp.txt", ios::in};
+    string out;
+    string *lines=nullptr;
+
+        if(!meth)
+        {
+            cout << "no such file!" << endl;
+            exit(1);
+        }
+
+    int linesnum=0;
+        while(getline(meth,line)){
+            linesnum++;
+        }
+    cout << linesnum;
+    lines = new string[linesnum];
+    int i=0;
+        while(getline(meth,line) && i<linesnum){
+            lines[i] = new string;
+            lines[i] = line;
+            i++;
+        }
+    meth.close();
+
+        for(int j=0;j<linesnum;j++){
+            cout << lines[j] << endl;
+        }
+}
+string getTarget(){
+    return "Amos";
 }
 /*
 void getMetodo(string metodo){
     string postfix=".txt";
     ofstream.open(strcat(metodo,postfix));
 }
-*/
+*//*
 string Color(int color, string Message){
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
     return Message;
 }
+*/
