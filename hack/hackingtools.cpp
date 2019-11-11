9d1b45831e49b88a84ba0397d100da02f7c2afb2
SteveSevetS <steve.joker00@gmail.com>
2019-11-11 09:19:22 +0100

    class Tagrget added

--- hack/hackingtools.cpp
+++ hack/hackingtools.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <string>
 
+#include "hackingtools.h"
+
 namespace hackingtools
 {
+
+    Target::Target(std::string name, int color)
+        :TName(name),
+        TColor(color)
+    {}
+
+    std::string Target::getTarget(){ return TName; }
+
+    int Target::getColor(){ return TColor; }
+
     void prova(std::string sas)
     {
         std::cout << sas << std::endl;
--- hack/hackingtools.h
+++ hack/hackingtools.h
@@ -1,7 +1,23 @@
 #include <fstream>
 #include <string>
 
+#ifndef HACKINGTOOLS_H
+#define HACKINGTOOLS_H
 namespace hackingtools
 {
+    class Target
+    {
+        public:
+            Target(std::string, int);
+            std::string getTarget();
+            int getColor();
+
+        private:
+            std::string TName;
+            int TColor;
+    };
+
     void prova(std::string);
 }
+
+#endif //HACKINGTOOLS_H
